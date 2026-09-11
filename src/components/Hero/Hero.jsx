e9d8e544821a4ee216a6a63c2ed0f40c2c1ffd16
nishat jahan <nishatjahanposhpa@gmail.com>
2026-09-11 22:34:02 +0600

    refactor:rename hero section designation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -327,7 +327,7 @@ const Hero = () => {
                      text-primary border border-primary/25"
         >
           <span>👩‍💻</span>
-          Jr. Full Stack Developer · Backend Focused 
+          Jr. Full Stack Developer · MERN Stack 
         </motion.div>
 
         {/* Name */}
